6d34c6bdd904f9f278f9341fc15cb1245f4b8e8d
Jasper <mvbrazas@gmail.com>
2023-06-23 12:12:52 +0800

    added storybook

---
+++ .ondevice/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+    stories: [
+      '../src/components/**/*.stories.tsx'
+    ],
+    addons: [
+      '@storybook/addon-ondevice-notes',
+      '@storybook/addon-controls',
+      '@storybook/addon-ondevice-backgrounds',
+      '@storybook/addon-actions',
+    ],
+  };
---
+++ src/components/SampleComponent/SampleComponent.stories.tsx
@@ -0,0 +1,14 @@
+import SampleComponent from "./SampleComponent";
+
+export default {
+    title: 'components/SampleComponent',
+    component: SampleComponent,
+};
+
+const Basic = {
+    args: {
+        label: "Myx Radio"
+    }
+};
+
+export { Basic }
